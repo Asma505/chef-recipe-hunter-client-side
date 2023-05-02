473c03a4c9a59ac9c46e240033516bc7f60bdf20
unknown <sonali.hcn@gmail.com>
2023-05-03 00:00:33 +0600

    code for login page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogin = event =>{
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const submit = form.submit.value;
+        console.log(email, password);
+    }
     return (
-        <div>
-            <h3>This is login</h3>
+        <div className='w-2/3 mx-auto my-52 bg-gray-300 py-10'>
+            <h3 className='text-center text-3xl font-bold'>Please Login</h3>
+            
+            <form onSubmit={handleLogin}>
+                <div className='text-center'>
+                    <label>Email</label>
+                    <input className='block w-1/2 mx-auto mb-5' type="email" name="email" id="" placeholder=' Your Email' required />
+
+
+                    <label>Password</label>
+                    <input className='block w-1/2 mx-auto mb-5' type="password" name="password" id="" placeholder=' Enter Password' required />
+                </div>
+
+                <div className='text-center'>
+                    <button className='w-1/2 mx-auto bg-teal-800 text-white py-2' type="submit" name='submit'>Login</button>
+                </div>
+            </form>
+
+            <div className='text-center'>
+                <button className='bg-teal-800 text-white py-2 w-1/2 mx-auto my-3'> Login with Google </button>
+            </div>
+
+            <div className='text-center'>
+                <button className='bg-teal-800 text-white py-2 w-1/2 mx-auto'> Login with Github </button>
+            </div>
+            <div className='text-center my-3 font-bold'>
+                <p>Don't Have an Account? <Link className='text-teal-800' to="/register">Register</Link></p>
+            </div>            
         </div>
     );
 };
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             
             <ActiveLink to='/blog' ><p className='p-5 text-white font-bold'>Blog</p></ActiveLink>
             
-            <button className='py-5 px-8 bg-white text-teal-800 rounded-full font-bold'><Link to='/login'>Login</Link></button>
+            <button className='py-5 px-3 bg-white text-teal-800 rounded-full font-bold'><Link to='/login'>Login</Link></button>
         </div>
     );
 };
