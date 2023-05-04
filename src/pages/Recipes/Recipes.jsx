6ebf96b4c05d97dfb5d75ba1a1cebad501955a00
unknown <sonali.hcn@gmail.com>
2023-05-04 16:24:56 +0600

    added recipes on Recipes page

--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -1,18 +1,65 @@
 import React from 'react';
-import { useLoaderData} from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
-const Recipes = () => {    
+const Recipes = () => {
     const chef = useLoaderData();
-    const {picture, name, bio, likes, num_of_recipes, years_of_experience} = chef;
+    const { picture, name, bio, likes, num_of_recipes, years_of_experience, food_name, ingredients, directions, image, rating } = chef;
     return (
-        <div className='bg-teal-600'>
-            <div className='w-1/2 mx-auto pb-5 mb-6 text-white'>
-                <img className='w-full mx-auto' src={picture} />
-                <p className='text-3xl py-3 font-bold'>Name: {name}</p>
-                <p className='font-semibold'>Number of Recipes: {num_of_recipes}</p>
-                <p className='mb-3 font-semibold'>Years of Experience: {years_of_experience} Years.</p>
-                <p className='text-xl mb-3'>Description: {bio}</p>
-                <p className='text-xl font-bold text-center'>Likes: {likes}</p>
+        <div >
+            <div className='bg-teal-700'>
+                <div className='w-1/2 mx-auto pb-5 text-white'>
+                    <img className='w-full mx-auto' src={picture} />
+                    <p className='text-3xl py-3 font-bold'>Name: {name}</p>
+                    <p className='font-semibold'>Number of Recipes: {num_of_recipes}</p>
+                    <p className='mb-3 font-semibold'>Years of Experience: {years_of_experience} Years.</p>
+                    <p className='text-xl mb-3'>Description: {bio}</p>
+                    <p className='text-xl font-bold text-center'>Likes: {likes}</p>
+                </div>
+            </div>
+            <p className='py-16 text-center text-3xl font-bold'>Recipes of {name}</p>
+            <div className='grid grid-cols-3 w-10/12 mx-auto'>
+                <div className="card w-80 glass">
+                    <figure><img src={image} /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Recipe Name: {food_name}</h2>
+                        <p><span className='font-bold'>Ingredients:</span> {ingredients}</p>
+                        <p><span className='font-bold'>Cooking method</span> {directions}</p>
+                        <div className='flex items-center font-bold'>
+                            <p>Rating: {rating}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn bg-teal-800">Favorite</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-80 glass">
+                    <figure><img src={image} alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Recipe Name: {food_name}</h2>
+                        <p><span className='font-bold'>Ingredients:</span> {ingredients}</p>
+                        <p><span className='font-bold'>Cooking method</span> {directions}</p>
+                        <div className='flex items-center font-bold'>
+                            <p>Rating: {rating}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn bg-teal-800">Favorite</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-80 glass">
+                    <figure><img src={image} alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Recipe Name: {food_name}</h2>
+                        <p><span className='font-bold'>Ingredients:</span> {ingredients}</p>
+                        <p><span className='font-bold'>Cooking method</span> {directions}</p>
+                        <div className='flex items-center font-bold'>
+                            <p>Rating: {rating}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn bg-teal-800">Favorite</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
